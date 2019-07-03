2ddfabca4f60d019130077dc2bac7a15d1c2b1c4
loenko <loenko.js@gmail.com>
2019-07-03 09:12:24 +0500

    Switch to exchangeratesapi, add date-selective request

--- apps/currency-convertor/src/app/data-fetcher/data-fetcher.service.ts
+++ apps/currency-convertor/src/app/data-fetcher/data-fetcher.service.ts
@@ -2,12 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+const API_URL = 'https://api.exchangeratesapi.io/'
+
 @Injectable()
 export class DataFetcherService {
 
   constructor(private http: HttpClient) { }
 
   getData(): Observable<any> {
-    return this.http.get('https://api.myjson.com/bins/1dnd5n');
+    return this.http.get(`${API_URL}latest`);
+  }
+
+  getDataByDate(date: string): Observable<any> {
+    return this.http.get(`${API_URL}${date}`);
   }
 }
